18c181c0bd3012b01e40329e6bea2e63ef36c837
hleeminh <leeminh2221@gmail.com>
2023-03-19 23:23:36 +0700

    first commit

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect} from 'react'
+import Wrapper from './Wrapper';
+import Link from 'next/link';
+
+const Header = () => {
+
+    const [mobileMenu, setMobileMenu] = useState(false);
+    const [showCatMenu, setShowCatMenu] = useState(false);
+    const [show, setShow] = useState('translate-y-0');
+    const [lastScrollY, setLastScrollY] = useState(0)
+
+    return (
+        <header 
+            className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20
+            sticky top-0 transition-transform duration-300 ${show}`}
+        >
+            <Wrapper>
+                <Link href='/'>
+                    <img src='/assets/logo.svg' className='w-[40px] 
+                    md:w-[60px]' alt="" />   
+                </Link>                
+            </Wrapper>        
+        </header>
+    )
+}
+
+export default Header
